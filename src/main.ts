298354aedfae74c6394a5b7a338c273322acbcef
Matin <matink1490@gmail.com>
2022-07-26 15:10:06 +0430

    Set api prefix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+
+    // Initalize swagger
     const config = new DocumentBuilder()
         .setTitle('Nest test project')
         .setDescription('Matin kafshdooz`s test nest backend project')
@@ -13,6 +15,8 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
 
+    // Set global prefix for api.
+    app.setGlobalPrefix('api/v1');
     await app.listen(3000);
 }
 
